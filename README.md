f9f47e24e5a1143bf919529308a99db69c81dd14
cbhubbard <hubbard8@gmail.com>
2021-03-21 22:17:20 -0500

    Small updates

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Cull-Web-UI
-Web application UI for the Cull application. Written using React-Redux
+Web application UI for the Cull application. Written using React-Redux with Inversify for DI and Redux-Observables to handle side effect flows
--- src/epics/user.epics.ts
+++ src/epics/user.epics.ts
@@ -1,66 +1,91 @@
 import { ofType, Epic, ActionsObservable, StateObservable } from 'redux-observable';
-import { from, of } from 'rxjs';
+import { of } from 'rxjs';
 import { map, catchError, switchMap } from 'rxjs/operators';
-import { registerRequest, registerError, registerSuccess, successAlert, loginRequest, errorAlert, loginFailure, loginSuccess, authRequest, authSuccess, authFailure, logoutRequest, logoutSuccess } from '../actions';
+import {
+    registerRequest,
+    registerError,
+    registerSuccess,
+    successAlert,
+    loginRequest,
+    errorAlert, 
+    loginFailure,
+    loginSuccess,
+    authRequest,
+    authSuccess,
+    authFailure, 
+    logoutRequest,
+    logoutSuccess
+} from '../actions';
+import { IAuthService, IUserService } from '../interfaces';
 import { User, Tokens } from '../models';
-import { UserService, history } from '../services';
+import { history } from '../services';
+import { SERVICE_IDENTIFIERS } from '../constants';
+import { useInjection } from '../hooks';
 
 // TODO: Remove anys
 export const register$: Epic<any> = (actions$: ActionsObservable<any>, state$: StateObservable<any>) => actions$.pipe(
     ofType(registerRequest),
     switchMap((user: User) => {
-        return from(UserService.register(user)).pipe(
+        // Retrieve the UserService -- can I use hooks in the epics?
+        const userService: IUserService = useInjection<IUserService>(SERVICE_IDENTIFIERS.IUSER_SERVICE);
+        return userService.register(user).pipe(
             map(_ => [
                 registerSuccess(user),
                 successAlert('Registration Success'),
                 loginRequest(user, false)
+            ]),
+            catchError(error => [
+                registerError(),
+                errorAlert('Registration Failed')
             ])
         );
-    }),
-    catchError(error => [
-        registerError(),
-        errorAlert('Registration Failed')
-    ])
+    })
 );
 
 export const login$: Epic<any> = (actions$: ActionsObservable<any>, state$: StateObservable<any>) => actions$.pipe(
     ofType(loginRequest),
     switchMap(({ user, checked }) => {
-        return from(UserService.login(user, checked)).pipe(
-            map((user: User) => {
+        const userService: IUserService = useInjection<IUserService>(SERVICE_IDENTIFIERS.IUSER_SERVICE);
+        return userService.login(user, checked).pipe(
+            map((user: Partial<User>) => {
                 history.push('/home');
                 return [
                     successAlert('Login Successful'),
                     loginSuccess(user),
                     authRequest(user)
                 ];
+            }),
+            catchError(error => {
+                console.error(error.toString());
+                return of(loginFailure);
             })
         )
-    }),
-    catchError(error => {
-        console.error(error.toString());
-        return of(loginFailure);
     })
 );
 
 export const auth$: Epic<any> = (actions$: ActionsObservable<any>, state$: StateObservable<any>) => actions$.pipe(
     ofType(authRequest),
-    switchMap((user: User) => {
-        return from(UserService.currentTokens()).pipe(
-            map((tokens: Tokens) => authSuccess(tokens))
+    switchMap(() => {
+        const authService: IAuthService = useInjection<IAuthService>(SERVICE_IDENTIFIERS.IAUTH_SERVICE);
+        return authService.currentTokens().pipe(
+            map((tokens: Tokens) => authSuccess(tokens)),
+            catchError(error => {
+                console.error(error.toString());
+                return of(authFailure);
+            })
         );
-    }),
-    catchError(error => {
-        console.error(error.toString());
-        return of(authFailure);
     })
 );
 
 export const logout$: Epic<any> = (actions$: ActionsObservable<any>, state$: StateObservable<any>) => actions$.pipe(
     ofType(logoutRequest),
-    map((_) => {
-        UserService.logout();
-        history.push('/home');
-        return logoutSuccess();
+    switchMap(() => {
+        const userService: IUserService = useInjection<IUserService>(SERVICE_IDENTIFIERS.IUSER_SERVICE);
+        return userService.logout().pipe(
+            map(() => {
+                history.push('/home');
+                return logoutSuccess();
+            })
+        );
     })
 );

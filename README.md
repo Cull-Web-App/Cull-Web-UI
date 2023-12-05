b7913136e447464b4e1c13d997abfd25b600ce97
Chris <cbhubbard8@gmail.com>
2023-12-05 12:52:15 -0600

    fix

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
@@ -45,7 +44,3 @@ You don’t have to ever use `eject`. The curated feature set is suitable for sm
 You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
 
 To learn React, check out the [React documentation](https://reactjs.org/).
-=======
-# Cull-Web-UI
-Web application UI for the Cull application. Written using React-Redux with Inversify for DI and Redux-Observables to handle side effect flows
->>>>>>> master
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import 'reflect-metadata';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -16,25 +15,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
-=======
-import * as React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { reducer as form } from 'redux-form';
-import { createEpicMiddleware, combineEpics } from 'redux-observable';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { App, InversifyProvider } from './components';
-import { login, registration, tokens } from './reducers';
-import { register$, login$, logout$, auth$ } from './epics';
-import { container } from './config';
-
-import './assets/Utilities.scss';
->>>>>>> master
 
 const epicMiddleWare = createEpicMiddleware();
 
 // Create the store using the combined reducers
-<<<<<<< HEAD
 const store = configureStore(
     {
         reducer: combineReducers({
@@ -44,21 +28,10 @@ const store = configureStore(
         }),
         middleware: [epicMiddleWare]
     }
-=======
-const store = createStore(
-    combineReducers({
-        login,
-        registration,
-        tokens,
-        form
-    }),
-    applyMiddleware(epicMiddleWare)
->>>>>>> master
 );
 
 epicMiddleWare.run(
     combineEpics(
-<<<<<<< HEAD
         initializeSymbols$,
         subscribe$,
         unsubscribe$
@@ -78,21 +51,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-=======
-        register$,
-        login$,
-        logout$,
-        auth$
-    )
-);
-
-// Render the top level app component -- provide the store and the inversify DI container
-render(
-    <InversifyProvider container={container}>
-        <Provider store={store}>
-            <App/>
-        </Provider>
-    </InversifyProvider>,
-    document.getElementById('app')
-);
->>>>>>> master

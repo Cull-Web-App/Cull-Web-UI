d3503ec9fb3d189811bf8204afc327ae0434f537
Chris <cbhubbard8@gmail.com>
2024-01-11 08:23:41 -0600

    virtualize the edit watch list for perf

--- src/features/right-panel/EditWatchList.component.tsx
+++ src/features/right-panel/EditWatchList.component.tsx
@@ -1,12 +1,13 @@
 import React, { memo, useEffect, useState } from 'react';
 import SearchBarComponent from './SearchBar.component';
 import { connect } from 'react-redux';
-import { clearAssetSearch, createOneWatch, deleteOneWatch, findManyAssetsWithQuery } from '../../state';
+import { clearAssetSearch, findManyAssetsWithQuery } from '../../state';
 import { faPlusCircle, faMinusCircle } from '@fortawesome/free-solid-svg-icons';
 import { IAsset, IWatch, StrictModeDroppable, Watch } from '../../common';
 import './EditWatchList.component.css';
 import EditWatchListItemComponent from './EditWatchListItem.component';
 import { DragDropContext, DropResult, DroppableProvided } from 'react-beautiful-dnd';
+import { AutoSizer, List, CellMeasurer, CellMeasurerCache, ListRowProps } from 'react-virtualized';
 
 type EditWatchListProps = EditWatchListDispatchProps & EditWatchListComponentProps & EditWatchListReduxProps;
 interface EditWatchListDispatchProps {
@@ -30,6 +31,12 @@ export const EditWatchListComponent = ({ clearSearch, findManyWithFilter, watchL
     const [rows, setRows] = useState<IWatch[]>([]);
     const [isAddMode, setIsAddMode] = useState(false);
     const [clearSearchCounter, setClearSearchCounter] = useState(0);
+    
+    // Create a cache that will store the height of each item
+    const cache = new CellMeasurerCache({
+        defaultHeight: 50, // Provide a default height for items
+        fixedWidth: true,
+    });
 
     useEffect(() => {
         if (searchResults.length === 0) {
@@ -90,18 +97,54 @@ export const EditWatchListComponent = ({ clearSearch, findManyWithFilter, watchL
         onRowUpdate(reorderedItem);
     };
 
+    const RenderRow = ({ index, key, parent, style }: ListRowProps) => {
+        const item = rows[index];
+        return (
+            <CellMeasurer
+                cache={cache}
+                columnIndex={0}
+                key={key}
+                parent={parent}
+                rowIndex={index}
+            >
+                {({ measure }) => (
+                    <div style={style} onLoad={measure}>
+                        <EditWatchListItemComponent
+                            key={item.symbol}
+                            watch={item}
+                            asset={assets.get(item.symbol)!}
+                            isAddMode={isAddMode}
+                            icon={isAddMode ? faPlusCircle : faMinusCircle}
+                            index={index}
+                            onClick={() => isAddMode ? handleAdd(item, watchList.length) : handleRemove(item)}
+                        />
+                    </div>
+                )}
+            </CellMeasurer>
+        );
+    };
+
     return (
         <div className='edit-modal'>
             <div className='edit-modal-header'>
-                <SearchBarComponent key={clearSearchCounter} expandEnabled={true} onSearchTermChange={handleSearchTermChanged} onSearch={() => {}}/>
+                <SearchBarComponent key={clearSearchCounter} expandEnabled={true} onSearchTermChange={handleSearchTermChanged} onSearch={() => { }} />
             </div>
             <DragDropContext onDragEnd={handleDragEnd}>
                 <StrictModeDroppable droppableId="watch-list">
                     {(provided: DroppableProvided) => (
                         <div className='edit-modal-rows' {...provided.droppableProps} ref={provided.innerRef}>
-                            {rows.map((item, index) => (
-                                <EditWatchListItemComponent key={item.symbol} watch={item} asset={assets.get(item.symbol)!} isAddMode={isAddMode} icon={isAddMode ? faPlusCircle : faMinusCircle} index={index} onClick={() => isAddMode ? handleAdd(item, watchList.length) : handleRemove(item)} />
-                            ))}
+                            <AutoSizer>
+                                {({ height, width }: { height: number, width: number }) => (
+                                    <List
+                                        height={height}
+                                        deferredMeasurementCache={cache}
+                                        rowHeight={cache.rowHeight}
+                                        rowCount={rows.length}
+                                        rowRenderer={RenderRow}
+                                        width={width}
+                                    />
+                                )}
+                            </AutoSizer>
                             {provided.placeholder}
                         </div>
                     )}
